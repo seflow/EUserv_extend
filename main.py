968d8b9f5d57c579c33e3236756cbf7456bafbf2
seflow <67130966+seflow@users.noreply.github.com>
2020-12-22 13:56:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ PROXIES = {
 }
 
 
+
 def login(username, password) -> (str, requests.session):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
